4177e96668678b3cd7608671c08c8b8f6438dfeb
Shawn Inder <shawninder@gmail.com>
2022-04-13 19:21:35 -0400

    Remove pollution + respect PHP style guide

--- functions.php
+++ functions.php
@@ -1,23 +1,16 @@
 <?php
 
-/**
- * Additional code for the child theme goes in here.
- */
-
 add_action('wp_enqueue_scripts', 'enqueue_child_styles', 99);
 add_action('wp_head', 'hook_javascript');
 
-function enqueue_child_styles()
-{
+function enqueue_child_styles() {
 	$css_creation = filectime(get_stylesheet_directory() . '/style.css');
-
 	wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', [], $css_creation);
 }
 
-function hook_javascript()
-{
+function hook_javascript() {
 ?>
-<!-- Fundraise Up: world-class checkout experience for serious online fundraising -->
+<!-- Begin Fundraise Up -->
 <script>(function(w,d,s,n,a){if(!w[n]){var l='call,catch,on,once,set,then,track'
 .split(','),i,o=function(n){return'function'==typeof n?o.l.push([arguments])&&o
 :function(){return o.l.push([n,arguments])&&o}},t=d.getElementsByTagName(s)[0],
