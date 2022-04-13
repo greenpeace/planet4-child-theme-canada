d550321cbc7795238acf01bcfcf07bfb8deaa79e
Shawn Inder <shawninder@gmail.com>
2022-04-13 15:15:27 -0400

    Add FundraiseUp script to `<head>`

--- functions.php
+++ functions.php
@@ -4,10 +4,27 @@
  * Additional code for the child theme goes in here.
  */
 
-add_action( 'wp_enqueue_scripts', 'enqueue_child_styles', 99);
+add_action('wp_enqueue_scripts', 'enqueue_child_styles', 99);
+add_action('wp_head', 'hook_javascript');
 
-function enqueue_child_styles() {
+function enqueue_child_styles()
+{
 	$css_creation = filectime(get_stylesheet_directory() . '/style.css');
 
-	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [], $css_creation );
+	wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', [], $css_creation);
+}
+
+function hook_javascript()
+{
+?>
+<!-- Fundraise Up: world-class checkout experience for serious online fundraising -->
+<script>(function(w,d,s,n,a){if(!w[n]){var l='call,catch,on,once,set,then,track'
+.split(','),i,o=function(n){return'function'==typeof n?o.l.push([arguments])&&o
+:function(){return o.l.push([n,arguments])&&o}},t=d.getElementsByTagName(s)[0],
+j=d.createElement(s);j.async=!0;j.src='https://cdn.fundraiseup.com/widget/'+a;
+t.parentNode.insertBefore(j,t);o.s=Date.now();o.v=4;o.h=w.location.href;o.l=[];
+for(i=0;i<7;i++)o[l[i]]=o(l[i]);w[n]=o}
+})(window,document,'script','FundraiseUp','AVGLZKMV');</script>
+<!-- End Fundraise Up -->
+<?php
 }
